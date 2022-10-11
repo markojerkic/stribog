cdaa049faf04065a41ab684ebf7d23249c77017a
Marko Jekrić <markojerkic266@gmail.com>
2022-10-11 19:25:12 +0200

    start filetree walker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let current_dir = env::current_dir().unwrap();
+    println!(
+        "Entries modified in the last 24 hours in {:?}:",
+        current_dir
+    );
+
+    for entry_res in fs::read_dir(current_dir).unwrap() {
+        let entry = entry_res.unwrap();
+        let path = entry.path();
+
+        let metadata = fs::metadata(&path).unwrap();
+        let last_modified = metadata.modified().unwrap().elapsed().unwrap().as_secs();
+
+        if last_modified < 5 * 24 * 3600 && metadata.is_file() {
+            println!(
+                "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
+                last_modified,
+                metadata.permissions().readonly(),
+                metadata.len(),
+                path.file_name().ok_or("No filename")
+            );
+        }
+    }
 }
