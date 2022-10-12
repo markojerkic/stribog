f8e3270d92bbc773110c42ef237570231e89b4db
Marko Jekrić <markojerkic266@gmail.com>
2022-10-12 09:27:46 +0200

    remove unused

--- src/main.rs
+++ src/main.rs
@@ -10,40 +10,10 @@ error_chain! {
     }
 }
 
-use walkdir::DirEntry;
-
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s.starts_with("."))
-        .unwrap_or(false)
-}
-
-fn list_dir(dir: &str, mut dir_vec: Vec<String>) -> Result<()> {
-    let walker = WalkDir::new(dir);
-
-    for entry in walker
-        .into_iter()
-        .filter(|entry| entry.is_ok())
-        .map(|entry| entry.unwrap())
-        .filter(|entry| is_hidden(entry))
-    {
-        let pntr = entry.path().display().to_string();
-        dir_vec.push(pntr);
-    }
-
-    Ok(())
-}
-
 fn is_whitelisted(file_name: &str, forbidden: &Vec<String>) -> bool {
     for forbidden_entry in forbidden {
         let forbidden_name = (*forbidden_entry).as_str();
         let starts_with = file_name.starts_with(forbidden_name);
-        // println!(
-        //     "Dir: {}, forbidden: {}, starts_with: {}",
-        //     file_name, forbidden_name, starts_with
-        // );
         if starts_with {
             return false;
         }
